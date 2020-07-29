8a7b903e100069c93bfe3010f3c82a8179d852ac
490273789 <wangshaonan1993@outlook.com>
2020-07-29 16:06:23 +0800

    项目初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-28 19:08:20
+ * @LastEditTime: 2020-07-29 13:57:33
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-template/src/views/Home.vue
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="mse"></div>
+    <button @click="initPlayer">play</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Player from 'xgplayer'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  name: 'Home',
+  data() {
+    return {
+      player1: null
+    }
+  },
+  methods: {
+    initPlayer() {
+      let id = document.querySelector('#mse')
+      console.log(id)
+      let player = new Player({
+        id: 'mse',
+        // url: "http://vcdn.e-eduspace.com/6c3c55c2bd834bca8663eac976f3d90c/6c3c55c2bd834bca8663eac976f3d90c.m3u8"
+        url: 'http://7vil13.com2.z0.glb.qiniucdn.com/XSASP0534.mp4'
+      })
+      this.player1 = player
+    }
   }
-};
+}
 </script>
+<style lang="scss" scope>
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
